57a5173dc02538898c2239d370e16e8da482bb72
Daniel Cortes <danncortes@gmail.com>
2018-02-11 20:24:25 -0500

    Creating test server post Todo

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,10 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
 describe('POST /Todos', () => {
     it('Should create a new todo', (done) => {
         const text = 'Test todo text';
@@ -24,6 +28,23 @@ describe('POST /Todos', () => {
                 Todo.find().then((todos) => {
                     expect(todos.length).to.equal(1);
                     expect(todos[0].text).to.deep.equal(text);
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+            });
+    });
+    it('Should not create todo with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).to.equal(0);
                     done();
                 }).catch((e) => {
                     done(e);
